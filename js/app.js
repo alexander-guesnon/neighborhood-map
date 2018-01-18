3383a2b9807815070b0582eb659d27fa33aee788
alexgooz <alexgooz92fs@gmail.com>
2018-01-18 02:54:06 -0800

    computed list using knockout

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 //somthing
 var ViewModle = function(){
-  
+  //modle
+  this.list = ko.observableArray([1,2,3,4,5]);
+
+  //control
+  this.printedList = ko.computed(function() {
+    var output ="";
+    for(var i =0 ; i< this.list().length; i++){
+      output+= "<li>"+ this.list()[i] +"</li>"
+    }
+    return output;
+  }, this);
 }
+console.log()
 ko.applyBindings(new ViewModle());
