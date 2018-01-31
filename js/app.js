5d26529ba67edf20b74e666b5e03e59ca82163c7
alexgooz <alexgooz92fs@gmail.com>
2018-01-30 17:17:27 -0800

    click hightlight

--- js/app.js
+++ js/app.js
@@ -1,20 +1,21 @@
-// TODO: make map fixed
-// TODO: make mobile freindly
 // TODO: errorhandle apis
 // TODO: comment code
+// TODO: README
 
 
 var markers = [];
 var map;
+var previousWindow;
+var previousitem;
 
 function populateInfoWindow(marker, infowindow) {
   // Check to make sure the infowindow is not already opened on this marker.
   if (infowindow.marker != marker) {
     infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>'+ '<a href=\"'+"https://www.google.com/maps/search/?api=1&query="+marker.position.lat()+", "+marker.position.lng()+""+'\">'+ "View on Google Maps" +'</a>' );
+    infowindow.setContent('<div>' + marker.title + '</div>' + '<a href=\"' + "https://www.google.com/maps/search/?api=1&query=" + marker.position.lat() + ", " + marker.position.lng() + "" + '\">' + "View on Google Maps" + '</a>');
     infowindow.open(map, marker);
     // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick',function(){
+    infowindow.addListener('closeclick', function() {
       infowindow.setMarker = null;
     });
 
@@ -24,7 +25,7 @@ function populateInfoWindow(marker, infowindow) {
 
 
 function initMap() {
-  var largeInfowindow = new google.maps.InfoWindow();
+  previousWindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
   map = new google.maps.Map(document.getElementById('map'), {
     center: {
@@ -45,7 +46,7 @@ function initMap() {
       });
       markers.push(tempmarker);
       tempmarker.addListener('click', function() {
-        populateInfoWindow(this, largeInfowindow);
+        populateInfoWindow(this, previousWindow);
       });
       bounds.extend(markers[i].position);
     }
@@ -55,45 +56,82 @@ function initMap() {
 
 var ViewModle = function() {
   var self = this;
-  self.center = ko.observable({lat: 37.786971, lng: -122.399677});
+  self.center = ko.observable({
+    lat: 37.786971,
+    lng: -122.399677
+  });
   self.activeLocation = ko.observableArray([]);
-  self.filter = function(){
-    var input = $("#textbox").val()
+  self.filter = function() {
+    var input = $("#textbox").val();
     for (var i = 0; i < self.activeLocation().length; i++) {
-      if(self.activeLocation()[i].title.indexOf(input) !== -1){
-          self.activeLocation.replace(self.activeLocation()[i],{title:self.activeLocation()[i].title, visable: true})
-          markers[i].setMap(map);
-      }else{
-          self.activeLocation.replace(self.activeLocation()[i],{title:self.activeLocation()[i].title, visable: false})
-          markers[i].setMap(null);
+      if (self.activeLocation()[i].title.indexOf(input) !== -1) {
+        self.activeLocation.replace(self.activeLocation()[i], {
+          title: self.activeLocation()[i].title,
+          visable: true,
+          hightlight: false
+        });
+        markers[i].setMap(map);
+      } else {
+        self.activeLocation.replace(self.activeLocation()[i], {
+          title: self.activeLocation()[i].title,
+          visable: false,
+          hightlight: false
+        });
+        markers[i].setMap(null);
       }
     }
   }
   self.GetData = function() {
     $.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=10000&gscoord=37.786971%7C-122.399677&format=json&callback=?", function(data) {
       for (var i = 0; i < data.query.geosearch.length; i++) {
-        var tempData = {title:data.query.geosearch[i].title,
-        visable: true};
+        var tempData = {
+          title: data.query.geosearch[i].title,
+          visable: true,
+          hightlight: false
+        };
         self.activeLocation.push(tempData)
       }
     });
   };
-  //close last window
-  var previousWindow;
-  self.select = function(){
-    if(previousWindow != null){
+  self.style = function(item) {
+    if (item == true) return 'hightlightList';
+    if (item == false) return 'normal';
+  }
+  var previousitem;
+  self.select = function() {
+    //close last window
+    if (previousWindow != null) {
       previousWindow.close();
     }
+    if (previousitem != null) {
+
+      for (var i = 0; i < self.activeLocation().length; i++) {
+        if (self.activeLocation()[i].title == previousitem.title) {
+          self.activeLocation.replace(self.activeLocation()[i], {
+            title: self.activeLocation()[i].title,
+            visable: self.activeLocation()[i].visable,
+            hightlight: false
+          });
+          break;
+        }
+
+      }
+    }
+    previousitem = this;
+    self.activeLocation.replace(previousitem, {
+      title: this.title,
+      visable: this.visable,
+      hightlight: true
+    });
     previousWindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
     for (var i = 0; i < markers.length; i++) {
-      if(markers[i].title === this.title){
+      if (markers[i].title === this.title) {
         populateInfoWindow(markers[i], previousWindow);
         break;
       }
 
     }
-    //TODO: hover animation or color change
   };
 };
 
